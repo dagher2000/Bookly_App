96af48454978f88e734d192ac4fb02a86c1e538d
Abdulkhaleq Dagher <abdodagher24@gmail.com>
2024-08-22 07:41:06 +0300

    create styles file and add text title

--- lib/features/home/data/presentation/views/widget/home_view_body.dart
+++ lib/features/home/data/presentation/views/widget/home_view_body.dart
@@ -1,5 +1,6 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widget/custom_app_bar.dart';
-import 'package:bookly_app/features/home/data/presentation/views/widget/custom_list_view_item.dart';
+import 'package:bookly_app/features/home/data/presentation/views/widget/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -7,30 +8,22 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        CustomAppBar(),
-        FeaturedBooksListView(),
-      ],
-    );
-  }
-}
-
-class FeaturedBooksListView extends StatelessWidget {
-  const FeaturedBooksListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8),
-              child: CustomListViewItem(),
-            );
-          }),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(),
+          FeaturedBooksListView(),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
