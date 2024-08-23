75e8077c241c131bccbee06e9f4fc530c4c4581c
Abdulkhaleq Dagher <abdodagher24@gmail.com>
2024-08-23 20:37:52 +0300

    create home repo & failure file

--- lib/features/splash/presentation/views/widget/splash_view_body.dart
+++ lib/features/splash/presentation/views/widget/splash_view_body.dart
@@ -1,6 +1,5 @@
 import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/asset.dart';
-import 'package:bookly_app/features/home/data/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widget/sliding_text.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
