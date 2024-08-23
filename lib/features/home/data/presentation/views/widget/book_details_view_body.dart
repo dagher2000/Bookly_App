26cc633eb84af351394c6d9c9fb990685f38afe2
Abdulkhaleq Dagher <abdodagher24@gmail.com>
2024-08-23 07:09:58 +0300

    create Books Action

--- lib/features/home/data/presentation/views/widget/book_details_view_body.dart
+++ lib/features/home/data/presentation/views/widget/book_details_view_body.dart
@@ -1,3 +1,7 @@
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/core/widget/custom_button.dart';
+import 'package:bookly_app/features/home/data/presentation/views/widget/book_rating.dart';
+import 'package:bookly_app/features/home/data/presentation/views/widget/books_action.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widget/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widget/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -14,9 +18,39 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.17),
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
             child: const CustomBookImage(),
           ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.titltextStyle30,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          const BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+          const SizedBox(
+            height: 37,
+          ),
+          const BooksAction(),
         ],
       ),
     );
