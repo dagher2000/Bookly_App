07dd010a569e6e4f8186c0b4a8ed2e2c96dd73a7
Abdulkhaleq Dagher <abdodagher24@gmail.com>
2024-08-23 18:18:43 +0300

    create similer books list view

--- lib/features/home/data/presentation/views/widget/book_details_view_body.dart
+++ lib/features/home/data/presentation/views/widget/book_details_view_body.dart
@@ -4,6 +4,7 @@ import 'package:bookly_app/features/home/data/presentation/views/widget/book_rat
 import 'package:bookly_app/features/home/data/presentation/views/widget/books_action.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widget/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/data/presentation/views/widget/custom_book_image.dart';
+import 'package:bookly_app/features/home/data/presentation/views/widget/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -51,6 +52,22 @@ class BookDetailsViewBody extends StatelessWidget {
             height: 37,
           ),
           const BooksAction(),
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SimilarBooksListView(),
         ],
       ),
     );
