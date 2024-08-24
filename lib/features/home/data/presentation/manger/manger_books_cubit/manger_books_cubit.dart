058d37c15239b42dfe514ac2e4d278882dfc2e22
Abdulkhaleq Dagher <abdodagher24@gmail.com>
2024-08-25 01:32:20 +0300

    create newest book cubit

--- lib/features/home/data/presentation/manger/manger_books_cubit/manger_books_cubit.dart
+++ lib/features/home/data/presentation/manger/manger_books_cubit/manger_books_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'manger_books_state.dart';
 
 class MangerBooksCubit extends Cubit<MangerBooksState> {
-  MangerBooksCubit() : super(MangerBooksInitial());
+  MangerBooksCubit(this.homeRepo) : super(MangerBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchFeaturedBooks() async {
+    emit(MangerBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold((failure) {
+      emit(MangerBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(MangerBooksSuccess(books));
+    });
+  }
 }
